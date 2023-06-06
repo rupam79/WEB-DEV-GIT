02666a5062f6767e6bd1470bccfe0459c377c3d4
rupam79 <rupamdhoundiyal47@gmail.com>
2023-06-06 12:40:34 +0530

    Learning JS

---
+++ JS/js/index.js
@@ -0,0 +1,49 @@
+//alert("this is working");      
+//variables
+var number1=43;
+var number2=22;
+alert(number1+number2);
+// console.log(number1+number2);
+
+/*data types in JS
+    1.primitive:undefined, null, number, string, boolean, symbol
+    2.reference:arrays, objects
+*/
+var str="this is string";
+var str2='this is other string';
+
+//objects
+var marks={
+    ravi:34,
+    subh:45,
+    harry:97
+}
+// console.log(marks);
+//booleans
+var a=true;
+var b=false;
+
+var und=undefined;
+//by default the value is undefined
+//null is different from undefined
+
+//arrays
+var array=[1,2,"hey"];
+// console.log(array);
+
+
+/*Operators in JS
+    1.arithmetic : + - * /
+    2.assignment : = -= += *= /=
+    3.comparison : == => =< !=
+    4.logical : && || not
+*/
+
+// FUNCTIONS 
+function avg(a,b){
+    return (a+b)/2;
+}
+// console.log(avg(10,15));
+
+//black : string
+//blue : number
